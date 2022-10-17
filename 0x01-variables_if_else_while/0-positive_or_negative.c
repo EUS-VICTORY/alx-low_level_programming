1d22bfa2d0b780618733028460ac0e398b9e67e7
EUS-VICTORY <cenessence72@gmail.com>
2022-10-17 11:46:15 -0700

    committing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,30 @@
-#!/bin/bash
-#int include <stdlib.h>
-#int include <time.h>
-#int include <stdio.h>
+/*
+**File: 0-positive_negative.c
+Auth: beth
+# !/ bin /bash
+
+#int include < stdlib.h >
+
+#int include < time.h >
+
+#int include < stdio.h >
+
 /**
 ** main - Prints a random number and states whether
 **it is positive , negative, or zero
 ** Return: Always 0;
-*/
+**/
 
-int main (void)
+int main(void)
 {
-    int n;
-    srand(time(0);
-    n = rand() - Rand_Max / 2;
-    if (n > 0)
-       printf("%d is positive\n", n);
-    else if (n < 0)	       
-       printf("%d is negative\n", n);
-    else		   
-       printf("%d is zero\n", n);
-    return(0);
-
+int n;
+srand(time(0);
+n = rand() - Rand_Max / 2;
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printf("%d is zero\n", n);
+return (0);
 }
-    
