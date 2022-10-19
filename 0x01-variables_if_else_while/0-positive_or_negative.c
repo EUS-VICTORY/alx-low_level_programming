53de9bf4bcf25bb829980749bf22a61c73d10037
EUS-VICTORY <cenessence72@gmail.com>
2022-10-19 01:03:13 -0700

    committing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 *Auth: beth
 */
 
-# !/ bin /bash
+#!/bin/bash
 
 #include <stdlib.h>
 
@@ -21,7 +21,7 @@ int main(void)
 {
 int n;
 srand(time(0));
-n = rand() - Rand_Max / 2;
+n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n < 0)
