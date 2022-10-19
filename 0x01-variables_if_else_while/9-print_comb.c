e5cceed13a6aa7940db8edec59ca564f4e157bfc
EUS-VICTORY <cenessence72@gmail.com>
2022-10-19 15:51:33 -0700

    committing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,18 +9,21 @@
 *You are not allowed to use any variable of type `char`.
 *Return: 0
 */
+
 int main(void)
+
 {
 int x;
 for (x = 48; x < 58; x++);
 {
 putchar(x);
 if (x < 57)
+}
 {
 putchar(44);
 putchar(32);	  
 }
-}
+{
 putchar('\n');
- return (0);
+return (0);
 }
