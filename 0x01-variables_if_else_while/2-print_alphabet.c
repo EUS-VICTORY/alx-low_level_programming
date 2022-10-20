6ad03b032972213e16c774324404121a91c20606
EUS-VICTORY <cenessence72@gmail.com>
2022-10-20 06:01:16 -0700

    committing

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,17 @@
 #include <time.h>
 #include <stdlib.h>
 /**
- * main : Program that prints lower case alphabets
+ * main : 
+ * Program that prints lower case alphabets
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
 {
-putchar(alphabet);    
+putchar(alphabet);
 }
 putchar('\n');
 return (0);
