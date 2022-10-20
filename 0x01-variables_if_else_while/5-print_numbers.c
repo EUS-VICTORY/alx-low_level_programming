a3535295696e2042c75cc8eaa168246527dde206
EUS-VICTORY <cenessence72@gmail.com>
2022-10-20 16:24:15 -0700

    committing

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -12,6 +12,7 @@ int main(void)
 {
 	int num;
 	for (num = 0; num < 10; num++)
+
 		printf("%i", num);
 	printf("\n");
 return (0);
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -19,7 +19,6 @@ int x;
 for (x = 0 ; x <= 9; x++);
 
 putchar(x +'0');
-if (x < 9)
 putchar(',');
 putchar(' ');
 putchar('\n');
