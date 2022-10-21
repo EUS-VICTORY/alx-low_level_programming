d1100e70513b4c1e55a724195329e5e30397aa05
EUS-VICTORY <cenessence72@gmail.com>
2022-10-20 18:37:47 -0700

    committing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -16,8 +16,8 @@ int main(void)
 int i;
 
 for (i = 48; i < 58; i++)
-putchar(i);																putchar(',');
-Putchar(' ');
+putchar(i);															putchar(',');
+putchar(' ');
 putchar('\n');
 return (0);
 }	
