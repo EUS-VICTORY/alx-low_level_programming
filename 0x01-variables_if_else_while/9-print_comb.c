913d6703dcd078073972545db2abf4641b4a84b9
EUS-VICTORY <cenessence72@gmail.com>
2022-10-20 17:31:46 -0700

    committing

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -17,8 +17,8 @@ int main(void)
 int num;
 
 for (num = 0; num <= 9; num++)
-putchar((num % 10) + '0');
-if (num <= 9)
+putchar(num + '0');
+if (num < 9)
 putchar(',');
 putchar(' ');
 putchar('\n');
