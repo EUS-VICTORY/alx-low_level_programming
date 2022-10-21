19cbe389d5eb63bfd78d67cdb6b6305821d0f63e
EUS-VICTORY <cenessence72@gmail.com>
2022-10-21 16:52:19 -0700

    committing

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - program that checks for lowercase character
+ * Returns: 1 if c is lower case, otherwise 0
+ */
+int _islower(int c)
+{
+	int i = 'a';
+for (c >= 'a'; c <= 'z';c++)
+{
+	/*if (c==i)*/
+	return (1);
+}
+{
+	return (0);
+}
+}
