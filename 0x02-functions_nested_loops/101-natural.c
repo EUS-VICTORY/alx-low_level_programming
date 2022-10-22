c06679b17311149624d630a44e2671023b6305a3
EUS-VICTORY <cenessence72@gmail.com>
2022-10-22 10:25:20 -0700

    committing

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include "nain.h"
+#include <stdio.h>
+/**
+ * -natural.c - prints all natural numbers that are multiples of 3 and 5
+ *  and sum them 
+ *  Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x < 1024, y, sum = 0
+		for (y = 0, y < x, y++)
+		{	if ((y % 3 == 0) || (y % 5 ==0))
+			{
+				sum = sum + y
+			}
+		}
+	printf("%d\n", sum);
+	return (0);
+}
