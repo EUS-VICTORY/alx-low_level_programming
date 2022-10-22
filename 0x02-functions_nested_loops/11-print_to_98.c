8508cc646faada88e3d18ac7f9615e7c93fbdb74
EUS-VICTORY <cenessence72@gmail.com>
2022-10-22 08:59:13 -0700

    committing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,12 +9,33 @@
  */
 void print_to_98(int n)
 {
-if (n <= 98)
-	for (n = n; n <= 97; n++)
-		printf("%d, ", n);
+if (n < 98)
+{
+while (n <= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
 }
-else 
-for (n = n; n > 98; n--)
-printf("%d, ", n);
-printf("98\n");
+n++;
+}
+}
+else if (n > 98)
+{
+while (n >= 98)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+n--;
+}
+}
+else
+{
+printf("98");
+}
+pintf("\n");
 }
