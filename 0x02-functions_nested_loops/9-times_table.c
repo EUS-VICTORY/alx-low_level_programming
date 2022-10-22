faaa4a316d3c6298a9c3ecd12a3339ecbc52b3be
EUS-VICTORY <cenessence72@gmail.com>
2022-10-22 06:32:28 -0700

    committing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,7 +7,6 @@
  */
 
 void times_table(void)
-
 {
 int row, column, product, tens, ones;
 for (row = 0; row <= 9; row++)
@@ -17,15 +16,13 @@ for (column = 0; column <= 9; column++)
 product = row * column;
 tens = product / 10;
 ones = product % 10;
-if (column == 0)
-											
+if (column == 0)									
 {
 _putchar('0');
 }
 else if (product < 10)
 {
 _putchar(',');
-_putchar(' ');
 _putchar(' ');
 _putchar(ones + '0');
 }
@@ -34,7 +31,6 @@ else
 _putchar(',');
 _putchar(' ');
 _putchar(tens + '0');
-_putchar(ones + '0');
 }
 {
 _putchar('\n');
