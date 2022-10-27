3d2dd9885ca47358d12c537c4023a4bf14b67c32
EUS-VICTORY <cenessence72@gmail.com>
2022-10-27 05:39:23 -0700

    committing

---
+++ 0x06-pointers_arrays_strings/0-_strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strcat - Function that concatenates two strings
+ * @*dest: destination string at the end of which the terminating null byte is overwritten
+ * @src:string to be appended
+ * Returns: pointer to resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int len = 0;
+	char *cpy;
+	for (; dest[len] != '\0'; len++)
+	cpy = dest + len;
+	while (*src != '\0')
+	*cpy = *src;
+	*cpy = '\0';
+	return (dest);
+}
