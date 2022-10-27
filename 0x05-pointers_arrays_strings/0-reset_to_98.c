c3e8d5432bab79515d9a666ffa3b15806affaa92
EUS-VICTORY <cenessence72@gmail.com>
2022-10-26 18:34:34 -0700

    committing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,8 +2,8 @@
 
 /**
 *reset_to_98 - function that takes a pointer to an int
-*@*n - parameter
-*Return: Always 0
+*@n - this is the pointer argument passed in.
+*Return: void.
 */
 
 void reset_to_98(int *n)
