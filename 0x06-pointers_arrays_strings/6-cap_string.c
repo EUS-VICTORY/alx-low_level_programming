0c1bd31de202c6a8616e22de0c65c903d934d7b7
EUS-VICTORY <cenessence72@gmail.com>
2022-10-27 13:54:06 -0700

    committing

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * cap_string - Function that capitalizes all words of a string
+ * separators of words: space, new line,tabulation,,, ;, ., !, ?, ", (, ), {, and }
+ * Return: Always 0
+ */
+char *cap_string(char *)
+{
+int len, j;
+char sep[13] = {' ', '\t', '\n', ',', ';', '.', '!',
+'?', '"', '(', ')', '{', '}'};
+for (len = 0; s[len] != '\0'; len++)
+{
+if (len == 0 && s[len] >= 97 && s[len] <= 122)
+s[len] -= 32;
+for (j = 0; j < 13; j++)
+if (s[len] == sep[j])
+if (s[len + 1] >= 97 && s[len + 1] <= 122)
+s[len + 1] -= 32;
+}
+return (s);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * Function that encodes a string into 1337
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ * Return: Always 0
+ */
+char *leet(char *)
+{
+int i, j;
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+if (s[i] == a[j])
+s[i] = b[j];
+}
+return (s);
+}
