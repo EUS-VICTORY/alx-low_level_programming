0cc1dac71daae0947ea666ba02f136481abcab87
EUS-VICTORY <cenessence72@gmail.com>
2022-10-30 16:00:32 -0700

    committing

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
-/**
- * main - function to add one line to a code
- * Return: a[2] = 98
- */
+#include <stdio.h
 
 int main(void)
 {
 int n;
 int a[5];
 int *p;
+
 a[2] = 1024;
 p = &n;
-
 /*
 * write your line of code here......
 * Remember:
