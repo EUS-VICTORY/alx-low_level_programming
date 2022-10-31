1600e1c167f14924b95567fdd9092aabc859b703
EUS-VICTORY <cenessence72@gmail.com>
2022-10-30 18:38:49 -0700

    committing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print_times_table - Prints a multiplication table up to param
+ * print_times_table - Prints n times table starting with 0 
  * @n: The number to be treated
  * Return: Number matrix
  */
