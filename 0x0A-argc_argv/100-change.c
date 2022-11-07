e6626e5115d7bec53d6fcb88eb2360cb475dc15f
EUS-VICTORY <cenessence72@gmail.com>
2022-11-07 10:20:15 -0800

    Argc, Argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int n, coins = 0;
-	/*validate input */
+	/* validate input */
 	if (argc != 2)
 	{
 		printf("error\n");
@@ -23,18 +23,18 @@ return (1);
 		printf("0\n");
 		return (0);
 	}
-	/* convert string to int and calculate coins */
-	n = atoi(argv[1]);
+/* convert string to int and calculate coins */
+n = atoi(argv[1]);
 
-	coins += n / 25;
-	n = n % 25;
-	coins += n / 10;
-		n = n % 10;
-		coins += n / 5;
-		n = n % 5;
-			coins += n / 2;
-			n = n % 2;
-			coins += n / 1;
+coins += n / 25;
+n = n % 25;
+coins += n / 10;
+n = n % 10;
+coins += n / 5;
+n = n % 5;
+coins += n / 2;
+n = n % 2;
+coins += n / 1;
 printf("%d\n", coins);
 return (0);
 }
