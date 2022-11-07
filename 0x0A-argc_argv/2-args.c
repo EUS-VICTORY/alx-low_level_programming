9bceeeddf821f9aea1141c54e4606de4a9019930
EUS-VICTORY <cenessence72@gmail.com>
2022-11-07 10:01:54 -0800

    Args, Argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints each argument passed to program on a new line, 
+ * including the first one
+ * @argc: argument counter
+ * @argv: pointer to array fo arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+
+int i;
+for (i = 0; i < argc; i++);
+printf("%s\n", argv[i]);
+
+return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * list of prototypes of all functions used
+ * Return: 0
+ */
+
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif /* MAIN_H */
