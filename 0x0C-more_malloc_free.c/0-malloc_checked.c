03fe4a8e4ba0fcb768506a325da1fee25c9e6c26
EUS-VICTORY <cenessence72@gmail.com>
2022-11-13 08:56:27 -0800

    More-malloc_free

--- 0x0C-more_malloc_free.c/0-malloc_checked.c
+++ 0x0C-more_malloc_free.c/0-malloc_checked.c
@@ -1,15 +1,23 @@
 #include "main.h"
-#include <stdio.h>                                                                          
-/**                                                                             * malloc_checked - main entry
-* @b: input                            
-* Return: 0
-*/                                                                                                                          
-void *malloc_checked(unsigned int b)                                                                                            
-{
 
-void *p;                                                                            
-p = malloc(b);                                                                                                          
-if (p == NULL)                                                                                                  
-exit(98);                                                                                               
-return (p);                                                                                                     
-}                               
+#include <stdlib.h>
+
+/**
+* malloc_checked - Main Entry
+* @b: input
+* Return: 0
+*/
+
+void *malloc_checked(unsigned int b)
+
+{
+void *p;
+
+p = malloc(b);
+
+if (p == NULL)
+
+exit(98);
+
+return (p);
+}
