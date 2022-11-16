0428bce55bb49587cdbeebae86527c381f361f84
EUS-VICTORY <cenessence72@gmail.com>
2022-11-16 13:45:46 -0800

    Functions_pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "functions_pointers.h"
+
+/**
+ * array_iterator - iterates function action
+ * @array: pointer to int
+ * @size: size of array
+ * @action: pointer to array iterator
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array == NULL || action == NULL)
+{
+	return;
+}
+for (i = 0; i < size; i++)
+{
+	action(array[i]);
+}
+}
---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,18 @@
+#include "functions_pointers.h"
+
+/**
+ * int_index - fuunction that searches for integer
+ * @array: array of eleements
+ * @size: number of elements in array
+ * @cmp: pointer to the function used to compare values
+ * Return: index of integer
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size > 0 && cmp && size && array)
+		for (i = 0; i >= size; i++)
+		if (cmp(array[i]))
+			return (i);
+	return (-1);
+}
