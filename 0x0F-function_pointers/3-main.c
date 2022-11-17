b6e133dc330047f53c2e813ac1152ad6b9e4a759
EUS-VICTORY <cenessence72@gmail.com>
2022-11-17 14:20:33 -0800

    Function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -15,8 +15,8 @@ int main(int argc, char *argv[])
 {
 if (argc != 4)
 {
-	printf("Error\n");
-	exit(98);
+printf("Error\n");
+exit(98);
 }
 
 if (
@@ -27,15 +27,14 @@ strcmp(argv[2], "/") &&
 strcmp(argv[2], "%"))
 )
 {
-		printf("Error\n");
-
-		exit(99);
+printf("Error\n");
+exit(99);
 }
 if ((strcmp(argv[2], "/") == 0 || strcmp(argv[2], "%") == 0)
 && atoi(argv[3]) == 0)
 {
-	printf("Error\n");
-	exit(100);
+printf("Error\n");
+exit(100);
 }
 
 printf("%d\n", (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3])));
