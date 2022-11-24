b9197ef2f1b4de6a17634f29c36b442440ae02ac
EUS-VICTORY <cenessence72@gmail.com>
2022-11-24 12:42:00 -0800

    repushing

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * list_len - Prints the number of elements in a linked list
+ * @h: pointer to the list
+ * Return: integer
+ **/
+
+size_t list_len(const list_t *h)
+{
+	const list *temp;
+	unsigned int counter = 0;
+	temp = h;
+
+	while (temp)
+	{
+		counter;
+		temp = temp->next;
+	}
+	return (counter);
+}
+
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end - Adds a new node at the end of the list
+ * @head: address of the first node of a list
+ * @str: address of the string to be inserted into the new node
+ * Retunr: address of new element(node)
+ * */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *temp, temp 2;
+	unsigned int len = 0;
+
+	if (str == NULL)
+		return (NULL);
+	temp = malloc(sizeof (list_t));
+	if (temp == 0)
+	return (NULL);
+	temp->str = strdup(str);
+	if (temp->str == NULL)
+	{
+		free(temp);
+	return (NULL);
+	}
+	while (str[length])
+	length++;
+	temp->len = length;
+	temp->next = NULL;
+	if (*head == NULL)
+	{
+		*head = temp;
+		return (temp);
+	}
+	temp 2 = *head;
+	while (temp2->next)
+	temp2 = temp2->next;
+	temp2->next = temp;
+	return (temp);
+}
