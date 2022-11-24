972ef1144a6bdce416fd8acee8ecf71bc501b04c
EUS-VICTORY <cenessence72@gmail.com>
2022-11-24 14:15:06 -0800

    linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,23 +1,31 @@
 #include "lists.h"
 
 /**
- * list_len - Prints the number of elements in a linked list
- * @h: pointer to the list
- * Return: integer
- **/
+ * list_len - Calculate the number of elements
+ * @h: Pointer to a list
+ * Return: Integer
+*/
 
 size_t list_len(const list_t *h)
+
 {
+
 	const list_t *temp;
+
 	unsigned int counter = 0;
 
 	temp = h;
 
 	while (temp)
-	{
-		counter;
-		temp = temp->next;
-	}
-	return (counter);
-}
 
+	{
+
+		counter++;
+
+		temp = temp->next;
+
+	}
+
+	return (counter);
+
+}
