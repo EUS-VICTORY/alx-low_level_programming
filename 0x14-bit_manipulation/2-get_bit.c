046dc6413c5b429e7135165a8a49bb1dd4046105
EUS-VICTORY <cenessence72@gmail.com>
2022-12-01 12:47:04 -0800

    Bit manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - gets the value of a bit at a given index
+ * @n: the bit
+ * @index: given index - indices start at 0
+ * Return: If error- -1, otherwise, 
+  	   the value of the bit at the index
+*/
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8)
+		return (-1);
+		if ((n & (1 << index) == 0)
+			return (0);
+
+			return (1);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets value of bit to 0 at a given index
+ * @n: A pointer to the bit
+ * @index: index at which to set value
+ * Return: 1 on success, and -1 if error
+ */
+nt clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+*n &= ~(1 << index);
+return (1);
+}
