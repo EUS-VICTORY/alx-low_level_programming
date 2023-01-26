1206026aafd8112130b787e818615dfbaef3939a
EUS-VICTORY <cenessence72@gmail.com>
2023-01-25 17:36:12 -0800

    Doubly-linked_lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	
 	if (!head)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
@@ -18,8 +19,11 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new->n = n;
 	new->next = head;
 	new->prev = NULL;
+	
 	if (*head)
+	  
 		(*head)->prev = new;
 	*head = new;
+	
 	return (new);
 }
