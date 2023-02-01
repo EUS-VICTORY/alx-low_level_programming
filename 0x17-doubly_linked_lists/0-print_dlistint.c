db2735096d364538a8f3c4fc02577382e2be7552
EUS-VICTORY <cenessence72@gmail.com>
2023-02-01 02:42:48 -0800

    Doubly-linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * print_dlistint - prints a doubly linked list
@@ -9,11 +10,12 @@
 size_t print_dlistint(const dlistint_t *h)
   
 {
+  const dlistint_t *ptr = h;
   size_t nodes = 0;
   
-  if (!h)
-    return (0);
-  while (h)
+  while (ptr && ptr->prev)
+    ptr = ptr->prev;
+  while (ptr)
     
     {
       printf("%d\n", h->n); 
